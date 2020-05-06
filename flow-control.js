4e8ace8c582e358d726f6886f644296ca4490893
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-06 00:18:28 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,7 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-  if (13 <= age <= 19) {
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else {
     return "You are not a teenager";
@@ -13,22 +13,23 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if (13 <= age <= 19) {
+  if (age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else if (age <= 12) {
     return "You are a kid";
-  } else if (age > 19) {
+  } else {
     return "You are a grownup";
   }
 }
 
 function ternaryTeenager(age) {
-  return 13 <= age <= 19 ?
+  return age >= 13 && age <= 19 ?
   "You are a teenager" :
   "You are not a teenager"
 }
 
 function switchAge(age) {
+  switch(age){
   case 13:
     console.log("You are a teenager");
     break;
@@ -52,4 +53,5 @@ function switchAge(age) {
     break;
   default:
     console.log("You have an age");
+}
 }
